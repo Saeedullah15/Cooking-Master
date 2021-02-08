df08541be5232612a4434747fb6be28088c3c4fe
Saeedullah15 <saeedullahazim65@gmail.com>
2021-02-08 23:06:15 +0600

    could not cook well

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-// .then(res => res.json())
-// .then(data => allMeal(data))
 
-// const allMeal = meals => {
-//     const searchField = document.getElementById("search-field");
-//     for (let i = 0; i < meals.length; i++) {
-//         const element = meals[i];
-//         const searchFieldDiv = document.createElement("div");
-//         const searchFieldInfo = `
-//             <input type="text" placeholder="search for meal">
-//             <button>Search</button>
-//         `
-//         searchFieldDiv.innerHTML = searchFieldInfo;
-//         searchField.appendChild(searchFieldDiv);        
-//     }
-    
-// }
-
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-.then(res => res.json())
-.then(data => allMeal(data))
-
-const allMeal = meals => {
-    const searchField = document.getElementById("search-field");
-    meals.forEach(element => {
-        const searchFieldDiv = document.createElement("div");
-            const searchFieldInfo = `
-                <input type="text" placeholder="search for meal">
-                <button>Search</button>
-            `
-            searchFieldDiv.innerHTML = searchFieldInfo;
-            searchField.appendChild(searchFieldDiv);
+const getSearchText = () => {
+    const searchText = document.getElementById("search-text").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayMeals(data.meals))
+}
+const displayMeals = meal => {
+    const mealContainer = document.getElementById("meals-container");
+    mealContainer.innerHTML = " ";
+    meal.forEach(element => {
+        const containerDiv = document.createElement("div");
+        containerDiv.className = "single-result row align-items-center my-3 p-3";
+        containerDiv.innerHTML = `
+        <div class="col-md-4">
+            <img src="${element.strMealThumb}" alt="">
+            <h4>${element.strMeal}</h3>
+            <a onclick="displayInstructions" href="">instructions</a>
+        </div>
+        `;
+        mealContainer.appendChild(containerDiv);
     });
+}
+const getInstructions = () => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${strInstructions}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayInstructions(data.meals.strInstructions))
+}
+const displayInstructions = instructions => {
+    const mealInstruction = document.getElementById("instructions");
+    mealInstruction.innerText = instructions;
 }
